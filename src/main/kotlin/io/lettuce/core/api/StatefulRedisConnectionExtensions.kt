e4464cd71ab8b9f460f1d4da280f0b16cb402002
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-03-06 14:18:43 +0200

    Fix kotlin extension

--- src/main/kotlin/io/lettuce/core/api/StatefulRedisConnectionExtensions.kt
+++ src/main/kotlin/io/lettuce/core/api/StatefulRedisConnectionExtensions.kt
@@ -3,13 +3,16 @@ package io.lettuce.core.api
 import io.lettuce.core.ExperimentalLettuceCoroutinesApi
 import io.lettuce.core.api.coroutines.RedisCoroutinesCommands
 import io.lettuce.core.api.coroutines.RedisCoroutinesCommandsImpl
+import io.lettuce.core.api.reactive.ReactiveStatefulRedisConnection
 
 /**
- * Extension for [StatefulRedisConnection] to create [RedisCoroutinesCommands]
+ * Extension for [ReactiveStatefulRedisConnection] to create [RedisCoroutinesCommands].
+ *
+ * Note: This extension requires Project Reactor on the classpath as it wraps the reactive API.
  *
  * @author Mikhael Sokolov
  * @author Mark Paluch
  * @since 6.0
  */
 @ExperimentalLettuceCoroutinesApi
-fun <K : Any, V : Any> StatefulRedisConnection<K, V>.coroutines(): RedisCoroutinesCommands<K, V> = RedisCoroutinesCommandsImpl(reactive())
+fun <K : Any, V : Any> ReactiveStatefulRedisConnection<K, V>.coroutines(): RedisCoroutinesCommands<K, V> = RedisCoroutinesCommandsImpl(reactive())
